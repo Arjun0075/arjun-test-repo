5756428c62101cc18a7ff26828d2b3a0db944593
Arjun <patilarjun2346@gmail.com>
2024-01-18 22:27:29 +0530

    BMI calculator added

--- test.js
+++ test.js
@@ -6,13 +6,13 @@
 //     alert(`THe entered name and email are ${name.value} and ${email.value}`)
 //     // console.log("Submit clicked")
 // }
-function handleClickOnSpan(event) {
-    console.log(event.target.className)
-    // let currEle =  document.querySelector(`.${event.target.className}`)
-    // currEle.style.background = "black"
-    console.log(event)
-    document.body.style.background = event.target.className
-}
+// function handleClickOnSpan(event) {
+//     console.log(event.target.className)
+//     let currEle =  document.querySelector(`.${event.target.className}`)
+//     currEle.style.background = "black"
+//     console.log(event)
+//     document.body.style.background = event.target.className
+// }
 
 // const purpleButton = document.querySelector(".purple")
 
@@ -20,3 +20,48 @@ function handleClickOnSpan(event) {
 //     console.log(event)
 //     document.body.style.background = "purple"
 // })
+
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit", (e)=>{
+    // debugger
+    e.preventDefault()
+    
+    // console.log(e.target)
+
+    let height = parseFloat(document.querySelector(".height").value)
+    let weight = parseFloat(document.querySelector(".weight").value) 
+    // console.log(height.value)
+
+    let BMI = (weight / (height* height)).toFixed(2)
+    if(!isNaN(BMI)){
+        let result = document.querySelector(".result")
+        console.log(result)
+        result.style.visibility = "visible"
+        result.innerHTML = `Your BMI is ${BMI}`
+    
+        let clearButton = document.querySelector(".clear-button")
+        clearButton.style.visibility = "visible"    
+    }else{
+        alert("Please enter correct Values")
+    }
+
+   
+    console.log(BMI)
+})
+
+function handleClear () {
+    let result = document.querySelector(".result")
+    result.style.visibility = "hidden"
+
+    let clearButton = document.querySelector(".clear-button")
+    clearButton.style.visibility = "hidden"
+
+    let height = document.querySelector(".height")
+    let weight = document.querySelector(".weight")
+
+    height.value = ""
+    weight.value = ""
+}
+   
